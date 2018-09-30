300fede925efccac978f2b4e0d39d5e41f944d9e
Nicoleyu <43199808+NicoleyuC@users.noreply.github.com>
2018-09-30 20:15:04 +0800

    Add files via upload

---
+++ fan09301.java
@@ -0,0 +1,11 @@
+public class fan09301 {
+
+    public static void main(String[] args) {
+        int data[] = {3, 53, 4, 68, 9};
+        for (int i = data.length-1 ; i >= 0 ; i--) {
+//                ▲這裡怎麼寫比較好?
+
+            System.out.print(data[i] + "\t");
+        }
+    }
+}
